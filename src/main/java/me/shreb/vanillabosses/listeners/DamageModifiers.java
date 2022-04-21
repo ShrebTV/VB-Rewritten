6780c872da2023c2bc133fc47c3e439d7b864bf5
Shreb <ShrebTV@gmail.com>
2022-04-21 12:26:40 +0200

    Added the DamageModifier class

---
+++ src/main/java/me/shreb/vanillabosses/listeners/DamageModifiers.java
@@ -0,0 +1,13 @@
+package me.shreb.vanillabosses.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class DamageModifiers implements Listener {
+
+    @EventHandler
+    public void onBossDamageEntity(){
+        //TODO
+    }
+
+}
