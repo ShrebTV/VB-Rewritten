3e07d92560668650227d210894802ba0cc2961dc
Shreb <ShrebTV@gmail.com>
2022-04-27 17:28:08 +0200

    implemented part of the Boss damage modifiers.

--- src/main/java/me/shreb/vanillabosses/listeners/DamageModifiers.java
+++ src/main/java/me/shreb/vanillabosses/listeners/DamageModifiers.java
@@ -1,13 +1,36 @@
 package me.shreb.vanillabosses.listeners;
 
+import me.shreb.vanillabosses.bosses.VBBoss;
+import me.shreb.vanillabosses.bosses.utility.BossDataRetriever;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class DamageModifiers implements Listener {
 
+    /**
+     * Event handler in order to modify damage dealt by bosses
+     * @param event the event to check for the damager being a boss in.
+     */
     @EventHandler
-    public void onBossDamageEntity(){
-        //TODO
-    }
+    public void onBossDamageEntity(EntityDamageByEntityEvent event) {
 
+        EntityType type = event.getDamager().getType();
+
+        if(!event.getDamager().getScoreboardTags().contains(VBBoss.BOSSTAG)) return;
+
+        BossDataRetriever retriever;
+        //TODO implement projectile damage modifiers
+        try {
+            retriever = new BossDataRetriever(type);
+        } catch(IllegalArgumentException ignored){
+            return;
+        }
+
+        double modifier = retriever.damageModifier;
+
+        event.setDamage(event.getFinalDamage() * modifier);
+
+    }
 }
