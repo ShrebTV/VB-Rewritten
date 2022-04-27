8f07c6d0a1291e0b514d4eb80082b7b362b4bcbd
Shreb <ShrebTV@gmail.com>
2022-04-27 17:23:47 +0200

    Added documentation to Boss.java

--- src/main/java/me/shreb/vanillabosses/bosses/bossRepresentation/Boss.java
+++ src/main/java/me/shreb/vanillabosses/bosses/bossRepresentation/Boss.java
@@ -14,9 +14,17 @@ public abstract class Boss {
     public int[] commandIndexes;
     public EntityType type;
 
+    /**
+     * A way to spawn the boss represented by this object.
+     * @param location the location to spawn the boss at
+     */
     abstract void spawnBoss(Location location);
 
-    public static void putCommandsToPDC(LivingEntity entity){
+    /**
+     * A Method to put the command PDC entry into the PDC of the entity specified
+     * @param entity the LivingEntity object to put the PDC entry on
+     */
+     public void putCommandsToPDC(LivingEntity entity){
         NormalBoss boss = new NormalBoss(entity.getType());
         entity.getPersistentDataContainer().set(BossCommand.COMMAND_INDEX_KEY, PersistentDataType.INTEGER_ARRAY, boss.commandIndexes);
     }
