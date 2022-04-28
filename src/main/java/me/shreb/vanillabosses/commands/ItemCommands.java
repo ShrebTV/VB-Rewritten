b58e626da1f6fe9766d4ed48b1dbdc4b0f04eca9
Shreb <ShrebTV@gmail.com>
2022-04-28 20:11:38 +0200

    Implemented registerCommand method, made class implement CommandExecutor

--- src/main/java/me/shreb/vanillabosses/commands/ItemCommands.java
+++ src/main/java/me/shreb/vanillabosses/commands/ItemCommands.java
@@ -1,6 +1,19 @@
 package me.shreb.vanillabosses.commands;
 
-public class ItemCommands extends VBCommands{
+import me.shreb.vanillabosses.Vanillabosses;
+import me.shreb.vanillabosses.logging.VBLogger;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.logging.Level;
+
+/**
+ * Command: vbItem
+ * Subcommands: - give
+ *              -
+ */
+public class ItemCommands extends VBCommands implements CommandExecutor {
 
     private static final ItemCommands INSTANCE = new ItemCommands();
 
@@ -9,7 +22,20 @@ public class ItemCommands extends VBCommands{
     }
 
     @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
+
+    @Override
     void registerCommand() {
+
+        try {
+            Vanillabosses.getInstance().getCommand("vbItem").setExecutor(getInstance());
+            new VBLogger("AdminCommands", Level.INFO, "Successfully registered command \"vbAdmin\"").logToFile();
+        } catch(NullPointerException e){
+            new VBLogger("AdminCommands", Level.SEVERE, "Could not register command \"vbAdmin\"").logToFile();
+        }
 
     }
 }
