9c5ca85365ca95c85fa2c9d85a1f72a8227b9d09
Shreb <ShrebTV@gmail.com>
2022-04-28 20:24:47 +0200

    Made constructor and set default values for the constructor. Added implementation for makeItem() and makeItem(int amount)

--- src/main/java/me/shreb/vanillabosses/items/BossEggs.java
+++ src/main/java/me/shreb/vanillabosses/items/BossEggs.java
@@ -3,9 +3,11 @@ package me.shreb.vanillabosses.items;
 import me.shreb.vanillabosses.Vanillabosses;
 import me.shreb.vanillabosses.bosses.utility.BossCreationException;
 import me.shreb.vanillabosses.bosses.utility.BossDataRetriever;
+import me.shreb.vanillabosses.items.utility.ItemCreationException;
 import me.shreb.vanillabosses.logging.VBLogger;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -22,15 +24,141 @@ public class BossEggs extends VBItem {
 
     public static BossEggs instance = new BossEggs();
 
-    public static NamespacedKey bossEggPDCKey = new NamespacedKey(Vanillabosses.getInstance(), "BossEgg");
+    public EntityType type;
+
+    private BossEggs() {
+        this.PDCKEY = new NamespacedKey(Vanillabosses.getInstance(), "BossEgg");
+        this.CONFIGSECTION = "BossEggs";
+    }
+
+    public BossEggs(EntityType type){
+        this.type = type;
+    }
 
     @Override
-    public ItemStack makeItem() {
+    public ItemStack makeItem() throws ItemCreationException {
+        ItemStack stack;
+        switch(this.type){
+
+            case BLAZE:
+                stack = new ItemStack(Material.BLAZE_SPAWN_EGG);
+                break;
+
+            case CREEPER:
+                stack = new ItemStack(Material.CREEPER_SPAWN_EGG);
+                break;
+
+            case ENDERMAN:
+                stack = new ItemStack(Material.ENDERMAN_SPAWN_EGG);
+                break;
+
+            case MAGMA_CUBE:
+                stack = new ItemStack(Material.MAGMA_CUBE_SPAWN_EGG);
+                break;
+
+            case SKELETON:
+                stack = new ItemStack(Material.SKELETON_SPAWN_EGG);
+                break;
+
+            case SLIME:
+                stack = new ItemStack(Material.SLIME_SPAWN_EGG);
+                break;
+
+            case SPIDER:
+                stack = new ItemStack(Material.SPIDER_SPAWN_EGG);
+                break;
+
+            case WITCH:
+                stack = new ItemStack(Material.WITCH_SPAWN_EGG);
+                break;
+
+            case WITHER:
+                stack = new ItemStack(Material.WITHER_SKELETON_SPAWN_EGG);
+                break;
+
+            case ZOMBIE:
+                stack = new ItemStack(Material.ZOMBIE_SPAWN_EGG);
+                break;
+
+            case ZOMBIFIED_PIGLIN:
+                stack = new ItemStack(Material.ZOMBIFIED_PIGLIN_SPAWN_EGG);
+                break;
+
+            default:
+                new VBLogger(getClass().getName(), Level.WARNING, "Error at Boss egg creation. Could not match EntityType passed in with a boss type!").logToFile();
+                throw new ItemCreationException("Could not make Boss egg: Type mismatch. Type passed in: " + type);
+        }
+
+//TODO edit PDC of the item
         return null;
     }
 
     @Override
-    void itemAbility(LivingEntity entity) {
+    public ItemStack makeItem(int amount) throws ItemCreationException {
+
+        ItemStack stack;
+        switch(this.type){
+
+            case BLAZE:
+                stack = new ItemStack(Material.BLAZE_SPAWN_EGG, amount);
+            break;
+
+            case CREEPER:
+                stack = new ItemStack(Material.CREEPER_SPAWN_EGG, amount);
+                break;
+
+            case ENDERMAN:
+                stack = new ItemStack(Material.ENDERMAN_SPAWN_EGG, amount);
+                break;
+
+            case MAGMA_CUBE:
+                stack = new ItemStack(Material.MAGMA_CUBE_SPAWN_EGG, amount);
+                break;
+
+            case SKELETON:
+                stack = new ItemStack(Material.SKELETON_SPAWN_EGG, amount);
+                break;
+
+            case SLIME:
+                stack = new ItemStack(Material.SLIME_SPAWN_EGG, amount);
+                break;
+
+            case SPIDER:
+                stack = new ItemStack(Material.SPIDER_SPAWN_EGG, amount);
+                break;
+
+
+            case WITCH:
+                stack = new ItemStack(Material.WITCH_SPAWN_EGG, amount);
+                break;
+
+
+            case WITHER:
+                stack = new ItemStack(Material.WITHER_SKELETON_SPAWN_EGG, amount);
+                break;
+
+
+            case ZOMBIE:
+                stack = new ItemStack(Material.ZOMBIE_SPAWN_EGG, amount);
+                break;
+
+
+            case ZOMBIFIED_PIGLIN:
+                stack = new ItemStack(Material.ZOMBIFIED_PIGLIN_SPAWN_EGG, amount);
+                break;
+
+            default:
+                new VBLogger(getClass().getName(), Level.WARNING, "Error at Boss egg creation. Could not match EntityType passed in with a boss type!").logToFile();
+                throw new ItemCreationException("Could not make Boss egg: Type mismatch. Type passed in: " + type);
+        }
+
+//TODO edit PDC of the item
+        return null;
+
+    }
+
+    @Override
+    public void itemAbility(LivingEntity entity) {
 
     }
 
@@ -47,9 +175,9 @@ public class BossEggs extends VBItem {
 
         if (itemStack == null || !itemStack.hasItemMeta()) return;
 
-        if (itemStack.getItemMeta().getPersistentDataContainer().has(bossEggPDCKey, PersistentDataType.STRING)) {
+        if (itemStack.getItemMeta().getPersistentDataContainer().has(PDCKEY, PersistentDataType.STRING)) {
 
-            EntityType type = EntityType.valueOf(itemStack.getItemMeta().getPersistentDataContainer().get(bossEggPDCKey, PersistentDataType.STRING));
+            EntityType type = EntityType.valueOf(itemStack.getItemMeta().getPersistentDataContainer().get(PDCKEY, PersistentDataType.STRING));
             BossDataRetriever bossData;
 
             try {
