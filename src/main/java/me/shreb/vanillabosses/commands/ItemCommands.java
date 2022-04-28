0068e2b0a204332857c72ae0e381e53db768975a
Shreb <ShrebTV@gmail.com>
2022-04-28 10:19:40 +0200

    Gave ItemCommands.java a private static final  instance, provided accessor

--- src/main/java/me/shreb/vanillabosses/commands/ItemCommands.java
+++ src/main/java/me/shreb/vanillabosses/commands/ItemCommands.java
@@ -1,8 +1,15 @@
 package me.shreb.vanillabosses.commands;
 
 public class ItemCommands extends VBCommands{
+
+    private static final ItemCommands INSTANCE = new ItemCommands();
+
+    public static ItemCommands getInstance(){
+        return INSTANCE;
+    }
+
     @Override
     void registerCommand() {
-        
+
     }
 }
