dc313ea2e33755cd5150a8ca4c31944dd7bb1077
Shreb <ShrebTV@gmail.com>
2022-04-28 20:06:18 +0200

    Fixed BossCommand.java the new placeholder %name% now signals that there may be multiple names the command should be executed for. Did actual implementation for commands being executed for multiple players BossCommand intended purpose now works!

--- src/main/java/me/shreb/vanillabosses/bosses/utility/BossCommand.java
+++ src/main/java/me/shreb/vanillabosses/bosses/utility/BossCommand.java
@@ -6,6 +6,7 @@ import me.shreb.vanillabosses.logging.VBLogger;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandException;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -14,7 +15,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.PluginManager;
 
 import java.util.*;
@@ -23,27 +23,40 @@ import java.util.stream.Collectors;
 
 public class BossCommand implements Listener {
 
+    static HashMap<Integer, String> commandMap = new HashMap<>();
+
     public static final NamespacedKey COMMAND_INDEX_KEY = new NamespacedKey(Vanillabosses.getInstance(), "CommandIndexes");
 
-    int index;
-    int delay;
-    String command;
-    int radius;
-    ArrayList<UUID> damagers;
-    ArrayList<UUID> playersToExecuteFor = new ArrayList<>();
+    public int index;
+    public int delay;
+    public String command;
+    public int radius;
+    public ArrayList<UUID> damagers = new ArrayList<>();
+    public ArrayList<UUID> playersToExecuteFor = new ArrayList<>();
 
     public static final String PLACEHOLDER_KILLER = "<killer>";
     public static final String PLACEHOLDER_DAMAGER = "<damager>";
     public static final String PLACEHOLDER_MOST_DAMAGE = "<mostDamage>";
 
-    static{
+    static {
         DamagerPHReplacer.cleanUp();
+
+        FileConfiguration config = Vanillabosses.getInstance().getConfig();
+        List<String> commandList = new ArrayList<>(config.getStringList("Bosses.CommandsExecutedOnBossDeath"));
+        for (String string : commandList) {
+            int i = commandList.indexOf(string);
+            commandMap.put(i, string);
+        }
+    }
+
+    public static HashMap<Integer, String> getCommandMap() {
+        return commandMap;
     }
 
     /**
      * registers all Listeners inside this class
      */
-    public static void registerListeners(){
+    public static void registerListeners() {
         PluginManager pm = Vanillabosses.getInstance().getServer().getPluginManager();
         Vanillabosses instance = Vanillabosses.getInstance();
         pm.registerEvents(new BossCommand(), instance);
@@ -51,7 +64,8 @@ public class BossCommand implements Listener {
         pm.registerEvents(new BossCommand.MostDamagePHReplacer(), instance);
     }
 
-    private BossCommand(){}
+    private BossCommand() {
+    }
 
     public BossCommand(int index, int delay, String command) {
         this.index = index;
@@ -68,7 +82,7 @@ public class BossCommand implements Listener {
 
         this.playersToExecuteFor.addAll(this.damagers);
 
-        if(this.radius > 0){
+        if (this.radius > 0) {
             playersToExecuteFor
                     .addAll(event.getEntity().getWorld()
                             .getNearbyEntities(event.getEntity().getLocation(), radius, radius, radius)
@@ -77,10 +91,25 @@ public class BossCommand implements Listener {
                             .collect(Collectors.toList()));
         }
 
-        try{
-            Bukkit.getServer().dispatchCommand(Vanillabosses.getInstance().getServer().getConsoleSender(), this.command);
-        } catch(CommandException e){
-            new VBLogger("BossCommand", Level.WARNING, "Attempted to execute a command. Execution failed. Command: " + command).logToFile();
+        if (playersToExecuteFor.isEmpty()) return;
+
+        for (UUID uuid : playersToExecuteFor.stream().distinct().collect(Collectors.toList())) {
+
+            Player player = Bukkit.getPlayer(uuid);
+            if (player == null) continue;
+
+            if (this.command.contains("%name%")) {
+                this.command = this.command.replace("%name%", player.getName());
+            }
+
+            try {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(Vanillabosses.getInstance(), () ->{
+                    Bukkit.getServer().dispatchCommand(Vanillabosses.getInstance().getServer().getConsoleSender(), this.command);
+
+                }, (long) delay * 20);
+            } catch (CommandException e) {
+                new VBLogger("BossCommand", Level.WARNING, "Attempted to execute a command. Execution failed. Command: " + command).logToFile();
+            }
         }
     }
 
@@ -96,7 +125,6 @@ public class BossCommand implements Listener {
             return;
         }
 
-
         if (this.command.contains(PLACEHOLDER_KILLER)) {
 
             new KillerPHReplacer().replaceKiller(this, event);
@@ -108,17 +136,15 @@ public class BossCommand implements Listener {
             new DamagerPHReplacer().setDamagers(this, event);
 
         }
-
         if (this.command.contains(PLACEHOLDER_MOST_DAMAGE)) {
-           UUID playerUUID = new MostDamagePHReplacer(uuid).getMostDamagePlayer();
-           Player player = Bukkit.getPlayer(playerUUID);
-           if(player != null){
-               this.command = this.command.replace(PLACEHOLDER_MOST_DAMAGE, player.getName());
-           } else {
-               new VBLogger("BossCommands", Level.WARNING, "Could not replace <mostDamage> Placeholder. Replacing with null. Command: " + this.command).logToFile();
-           }
-        }
+            UUID id = MostDamagePHReplacer.getMostDamageUUID(event.getEntity().getUniqueId());
 
+            Player player = Bukkit.getPlayer(id);
+            playersToExecuteFor.add(id);
+            this.command = this.command.replace(PLACEHOLDER_MOST_DAMAGE, "%name%");
+            if (player == null)
+                new VBLogger(getClass().getName(), Level.WARNING, "Could not replace <mostDamage> Placeholder. Player was null").logToFile();
+        }
         replaceRadiusPlaceHolder();
     }
 
@@ -128,11 +154,13 @@ public class BossCommand implements Listener {
      * Removes the "<>" if it exists
      */
     private void replaceRadiusPlaceHolder() {
+
         int radius;
 
         if (!this.command.contains("<")) return;
 
         String[] strings = this.command.split("<");
+
         if (strings.length != 2) {
             new VBLogger("PlaceHolders", Level.WARNING, "An Error occurred while replacing radius placeholder within command: " + this.command).logToFile();
             return;
@@ -165,7 +193,7 @@ public class BossCommand implements Listener {
             return;
         }
 
-        this.command = strings1[0] + strings1[1] + strings1[2];
+        this.command = strings[0] + "%name%" + strings1[2];
 
         this.radius = radius;
     }
@@ -194,23 +222,28 @@ public class BossCommand implements Listener {
 
             String killerName = killer.getName();
 
+            bossCommand.playersToExecuteFor.add(killer.getUniqueId());
+
             bossCommand.command = bossCommand.command.replace(PLACEHOLDER_KILLER, killerName);
         }
     }
 
-    static class DamagerPHReplacer implements Listener{
+    static class DamagerPHReplacer implements Listener {
 
         //This HashMap saves the UUID of the damaged boss along with a set of player UUIDs which were recognized as damagers
-        static HashMap<UUID, Set<UUID>> damagerHashMap = new HashMap<>();
+        static HashMap<UUID, ArrayList<UUID>> damagerHashMap = new HashMap<>();
 
-        private DamagerPHReplacer() {}
+        public DamagerPHReplacer() {
+        }
 
         public void setDamagers(BossCommand bossCommand, EntityDeathEvent event) {
+            bossCommand.damagers = new ArrayList<>();
 
-            if(!damagerHashMap.containsKey(event.getEntity().getUniqueId())) return;
+            if (!bossCommand.command.contains(PLACEHOLDER_DAMAGER)) return;
+            if (!damagerHashMap.containsKey(event.getEntity().getUniqueId())) return;
 
-            bossCommand.damagers = new ArrayList<>(damagerHashMap.get(event.getEntity().getUniqueId()));
-
+            bossCommand.damagers.addAll(damagerHashMap.get(event.getEntity().getUniqueId()));
+            bossCommand.command = bossCommand.command.replace(PLACEHOLDER_DAMAGER, "%name%");
         }
 
         //A way to keep track of bosses which are damaged by players.
@@ -224,33 +257,34 @@ public class BossCommand implements Listener {
                     || !damagedBoss.getScoreboardTags().contains(VBBoss.BOSSTAG)
                     || !(damager instanceof Player)) return;
 
-            if(!damagerHashMap.containsKey(damagedBoss.getUniqueId())){
-                damagerHashMap.put(damagedBoss.getUniqueId(), new HashSet<>());
+            if (!damagerHashMap.containsKey(damagedBoss.getUniqueId())) {
+                damagerHashMap.put(damagedBoss.getUniqueId(), new ArrayList<>());
             }
 
             damagerHashMap.get(damagedBoss.getUniqueId()).add(damager.getUniqueId());
-
         }
 
         /**
          * A method in order to not let the damagerHashMap grow out of proportion on big servers
          * Starts a repeating task for every 10 minutes in order to run through the damagerHashMap and remove dead entity UUIDs
          */
-        public static void cleanUp(){
+        public static void cleanUp() {
 
-            Bukkit.getScheduler().scheduleSyncRepeatingTask(Vanillabosses.getInstance(), ()-> {
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(Vanillabosses.getInstance(), () -> {
 
                 try {
-                    damagerHashMap.entrySet().stream().filter(n -> Bukkit.getEntity(n.getKey()) != null &&Bukkit.getEntity(n.getKey()).isDead()).forEach(n -> damagerHashMap.remove(n.getKey()));
+                    HashMap<UUID, ArrayList<UUID>> tempMap = new HashMap<>(damagerHashMap);
+
+                    tempMap.entrySet().stream().filter(n -> Bukkit.getEntity(n.getKey()) != null && Bukkit.getEntity(n.getKey()).isDead()).forEach(n -> damagerHashMap.remove(n.getKey()));
                     new VBLogger("DamagerPHReplacer", Level.INFO, "Cleaned up Boss damager map").logToFile();
-                } catch(NullPointerException e){
+                } catch (NullPointerException e) {
                     new VBLogger("DamagerReplacer", Level.WARNING, "An Error occurred while cleaning up the Boss Damager map. Please report this to the Author! \n Error: " + e).logToFile();
                 }
             }, 500, 20 * 60 * 10);
         }
     }
 
-    static class MostDamagePHReplacer implements Listener{
+    static class MostDamagePHReplacer implements Listener {
 
         // saves the boss UUID along with a MostDamagePHReplacer Object
         static HashMap<UUID, MostDamagePHReplacer> damagePHReplacerHashMap = new HashMap<>();
@@ -261,7 +295,8 @@ public class BossCommand implements Listener {
         private double bossHealth;
         private int registeredHits = 0; //How often this boss has been hit by players
 
-        private MostDamagePHReplacer(){}
+        private MostDamagePHReplacer() {
+        }
 
         public MostDamagePHReplacer(UUID bossUUID) {
             this.bossUUID = bossUUID;
@@ -269,23 +304,29 @@ public class BossCommand implements Listener {
             try {
                 this.type = Bukkit.getEntity(bossUUID).getType();
                 this.bossHealth = new BossDataRetriever(this.type).health;
-            } catch(NullPointerException ignored){
-            } catch(IllegalArgumentException e){
+            } catch (NullPointerException ignored) {
+            } catch (IllegalArgumentException e) {
                 new VBLogger(getClass().getName(), Level.WARNING, "Unexpected EntityType entered into a Damage Replacer. EntityType: " + this.type).logToFile();
             }
         }
 
         @EventHandler
-        public void onBossIsHit(EntityDamageByEntityEvent event){
+        public void onBossIsHit(EntityDamageByEntityEvent event) {
 
-            if(!event.getEntity().getScoreboardTags().contains(VBBoss.BOSSTAG)
-            || !(event.getDamager() instanceof Player)) {
+            if (!event.getEntity().getScoreboardTags().contains(VBBoss.BOSSTAG)
+                    || !(event.getDamager() instanceof Player) || !(event.getEntity() instanceof LivingEntity)) {
                 return;
             }
 
             UUID bossUUID = event.getEntity().getUniqueId();
+            BossDataRetriever retriever;
+            try {
+                retriever = new BossDataRetriever((LivingEntity) event.getEntity());
+            } catch (IllegalArgumentException e) {
+                new VBLogger(getClass().getName(), Level.WARNING, "An error occurred while registering damage dealt to boss. Error: " + e).logToFile();
+            }
 
-            if(!damagePHReplacerHashMap.containsKey(bossUUID)){
+            if (!damagePHReplacerHashMap.containsKey(bossUUID)) {
                 damagePHReplacerHashMap.put(bossUUID, new MostDamagePHReplacer(bossUUID));
             }
 
@@ -294,7 +335,7 @@ public class BossCommand implements Listener {
 
             damagePHReplacerHashMap.get(bossUUID).registeredHits++;
 
-            if(!damagePHReplacerHashMap.get(bossUUID).playerDamageMap.containsKey(playerUUID)){
+            if (!damagePHReplacerHashMap.get(bossUUID).playerDamageMap.containsKey(playerUUID)) {
                 damagePHReplacerHashMap.get(bossUUID).playerDamageMap.put(playerUUID, 0.0);
             }
 
@@ -303,25 +344,45 @@ public class BossCommand implements Listener {
             damagePHReplacerHashMap.get(bossUUID).playerDamageMap.put(playerUUID, prevDamage + damageDealt);
         }
 
-        public double getDamagePercentage(UUID playerUUID){
+        public double getDamagePercentage(UUID playerUUID) {
             double health = this.bossHealth;
             double damageByPlayer = damagePHReplacerHashMap.get(bossUUID).playerDamageMap.get(playerUUID);
 
-            return damageByPlayer/health;
+            return damageByPlayer / health;
         }
 
-        public int getRegisteredHits(){
+        public int getRegisteredHits() {
             return this.registeredHits;
         }
 
         /**
-         *  A Method to get the UUID of the player who did the most damage to the boss specified by this objects bossUUID
-         * @return The UUID of the player who has done the most damage to the boss.
+         * A Method to get the UUID of the player who did the most damage to the boss specified by this objects bossUUID
          */
-        public UUID getMostDamagePlayer(){
-            Map.Entry<UUID, Double> entry = this.playerDamageMap.entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
-            if(entry == null) return null;
-            return entry.getKey();
+        public static UUID getMostDamageUUID(UUID bossUUID) {
+
+            MostDamagePHReplacer replacer = damagePHReplacerHashMap.get(bossUUID);
+            Map.Entry<UUID, Double> entry = null;
+
+            Vanillabosses.getInstance().getServer().getConsoleSender().sendMessage(String.valueOf(replacer.playerDamageMap));
+
+            for (Map.Entry<UUID, Double> tmpEntry : replacer.playerDamageMap.entrySet()) {
+                if (entry == null || entry.getValue() < tmpEntry.getValue()) {
+                    entry = tmpEntry;
+                }
+            }
+
+            if (entry == null) return null;
+
+            UUID id = entry.getKey();
+            System.out.println(id);
+            Player player = Bukkit.getPlayer(id);
+            if (player == null) {
+                new VBLogger("BossCommands", Level.WARNING, "Player gotten from replaceMostDamage was null.\n" +
+                        "Information:\n" +
+                        "UUID: " + id + "\n" +
+                        "Entry: " + entry).logToFile();
+            }
+            return id;
         }
     }
 }
