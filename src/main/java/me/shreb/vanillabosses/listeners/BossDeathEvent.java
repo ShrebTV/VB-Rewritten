1489629806060122156ee21d2d62950fff8c6b20
Shreb <ShrebTV@gmail.com>
2022-04-28 20:25:22 +0200

    Implemented boss commands on boss death, removed TODO

--- src/main/java/me/shreb/vanillabosses/listeners/BossDeathEvent.java
+++ src/main/java/me/shreb/vanillabosses/listeners/BossDeathEvent.java
@@ -1,11 +1,15 @@
 package me.shreb.vanillabosses.listeners;
 
+import me.shreb.vanillabosses.bosses.utility.BossCommand;
 import me.shreb.vanillabosses.bosses.utility.BossDataRetriever;
 import me.shreb.vanillabosses.bosses.utility.BossDrops;
+import me.shreb.vanillabosses.logging.VBLogger;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.util.logging.Level;
 
 public class BossDeathEvent implements Listener {
 
@@ -18,7 +22,7 @@ public class BossDeathEvent implements Listener {
 
         try {
             bossData = new BossDataRetriever(entity);
-        } catch(IllegalArgumentException ignored){
+        } catch (IllegalArgumentException ignored) {
             return;
         }
 
@@ -26,7 +30,20 @@ public class BossDeathEvent implements Listener {
 
         BossDrops.dropItemStacks(drops.toItemStacks(), event.getEntity().getLocation());
 
-        //TODO implement boss commands here
+        for (int i : bossData.commandIndexes) {
+            String[] strings = BossCommand.getCommandMap().get(i).split("DELAY:");
 
+            if (strings.length != 2) {
+                new VBLogger(getClass().getName(), Level.WARNING, "Bad Command, 'DELAY:' was found more than one time or not at all. \n" +
+                        "If you do not want to have a delay to the command, please use 'DELAY: 0'\n" +
+                        "Command: " + BossCommand.getCommandMap().get(i)).logToFile();
+                continue;
+            }
+
+            int delay = Integer.parseInt(strings[1]);
+
+            BossCommand command = new BossCommand(i, delay, strings[0]);
+            command.executeBossCommand(event);
+        }
     }
 }
