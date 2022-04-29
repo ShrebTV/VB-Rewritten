815f17054147bc96ec2c2f2118b0291c5f9e7457
Shreb <ShrebTV@gmail.com>
2022-04-29 13:44:45 +0200

    Removed implementation for delay detection inside commands. moved code to setDelay() method inside BossCommand. setDelay() is called implicitly when making a new BossCommand object

--- src/main/java/me/shreb/vanillabosses/listeners/BossDeathEvent.java
+++ src/main/java/me/shreb/vanillabosses/listeners/BossDeathEvent.java
@@ -1,15 +1,14 @@
 package me.shreb.vanillabosses.listeners;
 
+import me.shreb.vanillabosses.bosses.bossRepresentation.RespawningBoss;
 import me.shreb.vanillabosses.bosses.utility.BossCommand;
 import me.shreb.vanillabosses.bosses.utility.BossDataRetriever;
 import me.shreb.vanillabosses.bosses.utility.BossDrops;
-import me.shreb.vanillabosses.logging.VBLogger;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
-
-import java.util.logging.Level;
+import org.bukkit.persistence.PersistentDataType;
 
 public class BossDeathEvent implements Listener {
 
@@ -34,40 +33,28 @@ public class BossDeathEvent implements Listener {
         //Drop the items from the BossDrops object after converting them toItemStacks()
         BossDrops.dropItemStacks(drops.toItemStacks(), event.getEntity().getLocation());
 
-        //iterate over the bossData.commandIndexes gotten from config. This contains the indexes of the commands which are supposed to be executed
-        for (int i : bossData.commandIndexes) {
-            //Separate command from Delay integer
-            String[] strings = BossCommand.getCommandMap().get(i).split("DELAY:");
-
-            //make sure the strings array has 2 objects. If it doesn't the Command string was faulty
-            if (strings.length != 2) {
-                new VBLogger(getClass().getName(), Level.WARNING, "Bad Command, 'DELAY:' was found more than one time or not at all. \n" +
-                        "If you do not want to have a delay to the command, please use 'DELAY: 0'\n" +
-                        "Command: " + BossCommand.getCommandMap().get(i)).logToFile();
-                //go to next command, which may not be faulty, thus not break;
-                continue;
+        //Make this section only applies for normal bosses, not respawning bosses
+        if (!entity.getScoreboardTags().contains(RespawningBoss.RESPAWNING_BOSS_TAG)) {
+            //iterate over the bossData.commandIndexes gotten from config. This contains the indexes of the commands which are supposed to be executed
+            for (int i : bossData.commandIndexes) {
+                //Make a new BossCommand object using the index of the command, the delay intended and the command String which is supposed to be executed
+                BossCommand command = new BossCommand(i);
+                //replace and read all placeholders, add necessary players to a list to execute the commands for, then execute the command for the players in that list
+                command.executeBossCommand(event);
             }
+        } else if (entity.getPersistentDataContainer().has(RespawningBoss.RESPAWNING_BOSS_PDC, PersistentDataType.INTEGER_ARRAY)
+                && entity.getPersistentDataContainer().get(RespawningBoss.RESPAWNING_BOSS_PDC, PersistentDataType.INTEGER_ARRAY) != null) {
 
-            //Parse the second object inside the String array to get the intended delay
-            int delay;
-            try {
-                delay = Integer.parseInt(strings[1]);
-            } catch(NumberFormatException e){
-                //log to the file and default to the value '0' in case the delay cannot be read
-                new VBLogger(getClass().getName(), Level.WARNING, "Could not read delay from command string. Defaulting to 0. Command: " + BossCommand.getCommandMap().get(i)).logToFile();
-                delay = 0;
+            int[] indexes = entity.getPersistentDataContainer().get(RespawningBoss.RESPAWNING_BOSS_PDC, PersistentDataType.INTEGER_ARRAY);
+
+            if(indexes == null) return;
+
+            for (int i : indexes) {
+                //Make a new BossCommand object using the index of the command, the delay intended and the command String which is supposed to be executed
+                BossCommand command = new BossCommand(i);
+                //replace and read all placeholders, add necessary players to a list to execute the commands for, then execute the command for the players in that list
+                command.executeBossCommand(event);
             }
-
-            //additional check for negative values
-            if(delay < 0) {
-                delay = 0;
-                new VBLogger(getClass().getName(), Level.WARNING, "Read a negative value for Delay of the command. Defaulting to 0. Command: " + BossCommand.getCommandMap().get(i)).logToFile();
-            }
-
-            //Make a new BossCommand object using the index of the command, the delay intended and the command String which is supposed to be executed
-            BossCommand command = new BossCommand(i, delay, strings[0]);
-            //replace and read all placeholders, add necessary players to a list to execute the commands for, then execute the command for the players in that list
-            command.executeBossCommand(event);
         }
     }
 }
