5453e04d962eb17f6b2ea025d3c1f64eebaa9d04
Sebastian Werberger <sebastian.werberger@stud.th-rosenheim.de>
2022-05-04 10:39:39 +0200

    Created WitherEgg.java

---
+++ src/main/java/me/shreb/vanillabosses/items/WitherEgg.java
@@ -0,0 +1,33 @@
+package me.shreb.vanillabosses.items;
+
+import me.shreb.vanillabosses.items.utility.ItemCreationException;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+
+public class WitherEgg extends VBItem{
+    @Override
+    public void itemAbility(LivingEntity entity) {
+
+    }
+
+    @Override
+    public ItemStack makeItem() throws ItemCreationException {
+        return null;
+    }
+
+    @Override
+    public ItemStack makeItem(int amount) throws ItemCreationException {
+        return null;
+    }
+
+    @Override
+    public void registerListener() {
+
+    }
+
+    @Override
+    <T extends Event> void itemAbility(T e) {
+
+    }
+}
