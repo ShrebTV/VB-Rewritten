43dfcc9c9d38d90fba2eccad4c8d03f37bb6e2ba
Sebastian Werberger <sebastian.werberger@stud.th-rosenheim.de>
2022-05-04 10:04:51 +0200

    Made and implemented listeners which handle the Skeletor plugin item. Still have to test, whether this works

--- src/main/java/me/shreb/vanillabosses/items/Skeletor.java
+++ src/main/java/me/shreb/vanillabosses/items/Skeletor.java
@@ -2,24 +2,29 @@ package me.shreb.vanillabosses.items;
 
 import me.shreb.vanillabosses.Vanillabosses;
 import me.shreb.vanillabosses.items.utility.ItemCreationException;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.entity.LivingEntity;
+import me.shreb.vanillabosses.logging.VBLogger;
+import org.bukkit.*;
+import org.bukkit.entity.*;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 public class Skeletor extends VBItem {
 
     public static Skeletor instance = new Skeletor();
 
-    public Skeletor(){
+    public Skeletor() {
         this.pdcKey = new NamespacedKey(Vanillabosses.getInstance(), "Skeletor");
         this.configSection = "Skeletor";
         this.itemMaterial = Material.BOW;
@@ -64,6 +69,7 @@ public class Skeletor extends VBItem {
     @Override
     public void registerListener() {
         pluginManager.registerEvents(this, Vanillabosses.getInstance());
+        pluginManager.registerEvents(new SkeletorArrow(), Vanillabosses.getInstance());
     }
 
     @Override
@@ -75,5 +81,176 @@ public class Skeletor extends VBItem {
     @Override
     <T extends Event> void itemAbility(T e) {
 
+    }
+
+    static class SkeletorArrow implements Listener {
+
+        //TODO Test this
+
+        public static final String EXPLODING_ARROW = "Vanilla Bosses - Exploding Arrow";
+        public static final String CANCEL_TNT_EXPLOSION = "Vanilla Bosses - Cancel Explosion";
+
+        /**
+         * Edits a normal arrow to have the tag needed for the Skeletor ability
+         *
+         * @param event the event to edit the arrow inside of
+         */
+        @EventHandler
+        public void makeSkeltorArrow(ProjectileLaunchEvent event) {
+
+            if (!(event.getEntity().getShooter() instanceof LivingEntity)) return;
+
+            LivingEntity shooter = (LivingEntity) event.getEntity().getShooter();
+            Projectile projectile = event.getEntity();
+
+            //make sure the projectile is an arrow
+            boolean isArrow = event.getEntity() instanceof Arrow;
+
+            //make sure the shooter of the projectile is a Living Entity and has a skeletor in their main hand
+            boolean shouldEdit = shooter != null
+                    && shooter.getEquipment() != null
+                    && shooter.getEquipment().getItemInMainHand().getType() != Material.AIR
+                    && shooter.getEquipment().getItemInMainHand().getItemMeta() != null
+                    && shooter.getEquipment().getItemInMainHand().getItemMeta().getPersistentDataContainer().has(Skeletor.instance.pdcKey, PersistentDataType.STRING);
+
+            if (isArrow) {
+                Arrow arrow = (Arrow) projectile;
+
+                if (shouldEdit) {
+                    arrow.getScoreboardTags().add(EXPLODING_ARROW);
+                }
+            }
+        }
+
+        /**
+         * Edits an arrow from a projectile launch event to a primed tnt block with the same vector and velocity
+         *
+         * @param event the event to edit the arrow inside of
+         */
+        @EventHandler
+        public void editArrowToTNT(ProjectileLaunchEvent event) {
+
+            if(!config.getBoolean("Items.Skeletor.ShootTNTFromOffHand.enabled")) return;
+
+            boolean isArrow = event.getEntity() instanceof Arrow;
+
+            boolean shooterIsAlive = event.getEntity().getShooter() instanceof LivingEntity;
+
+            if (isArrow && shooterIsAlive) {
+
+                Arrow arrow = (Arrow) event.getEntity();
+                LivingEntity shooter = (LivingEntity) event.getEntity().getShooter();
+
+                boolean shooterHoldingSkeletorMain = shooter != null
+                        && shooter.getEquipment() != null
+                        && shooter.getEquipment().getItemInMainHand().getType() != Material.AIR
+                        && shooter.getEquipment().getItemInMainHand().getItemMeta() != null
+                        && shooter.getEquipment().getItemInMainHand().getItemMeta().getPersistentDataContainer().has(Skeletor.instance.pdcKey, PersistentDataType.STRING);
+
+                if(!shooterHoldingSkeletorMain) return;
+
+                boolean shooterHoldingTNTOffHand = (shooter.getEquipment().getItemInOffHand().getType() == Material.TNT);
+
+                if(shooterHoldingTNTOffHand){
+
+                    if(shooter instanceof Player && ((Player) shooter).getGameMode() == GameMode.SURVIVAL){
+                        //decrease amount of TNT in off hand only if the shooter is a player and they are in survival mode
+                        shooter.getEquipment().getItemInOffHand().setAmount(shooter.getEquipment().getItemInOffHand().getAmount() - 1);
+                    }
+
+                    int fuseTicks = config.getInt("Items.Skeletor.ShootTNTFromOffHand.TNTTimer");
+                    double tntYield = config.getInt("Items.Skeletor.ShootTNTFromOffHand.TNTYield");
+
+                    Vector vector = arrow.getVelocity();
+
+                    arrow.remove();
+
+                    TNTPrimed tnt = (TNTPrimed) arrow.getLocation().getWorld().spawnEntity(arrow.getLocation(), EntityType.PRIMED_TNT);
+
+                    if (fuseTicks < 1) {
+                        new VBLogger(getClass().getName(), Level.WARNING, "Skeletor TNT Fuse ticks were set too low. 20 ticks is one second, cannot be less than 1 tick. Ticks: " + fuseTicks).logToFile();
+                    } else {
+                        tnt.setFuseTicks(fuseTicks);
+                    }
+
+                    if (tntYield < 0) {
+                        new VBLogger(getClass().getName(), Level.WARNING, "Skeletor TNT yield was set too low. Cannot be less than 0. Yield: " + tntYield).logToFile();
+                    } else {
+                        tnt.setYield((float) tntYield);
+                    }
+
+                    tnt.setVelocity(vector);
+                }
+            }
+        }
+
+        /**
+         * An event to listen to the ProjectileHitEvent in order to summon tnt on entities hit by a SkeletorArrow
+         * and edit it to the correct Attributes from the config
+         *
+         * @param event the event to spawn the tnt inside of
+         */
+        @EventHandler
+        public void onExplodingArrowHit(ProjectileHitEvent event) {
+
+            if (config.getBoolean("Items.Skeletor.TNTOnArrowHit.enable")) return;
+
+            Projectile projectile = event.getEntity();
+
+            boolean isSkeletorArrow = projectile instanceof Arrow
+                    && projectile.getScoreboardTags().contains(EXPLODING_ARROW);
+
+            boolean hitLivingEntity = event.getHitEntity() instanceof LivingEntity;
+
+            if (isSkeletorArrow && hitLivingEntity) {
+
+                ((Arrow) projectile).setDamage(0);
+                projectile.setFireTicks(1);
+                projectile.remove();
+
+                Location location = projectile.getLocation();
+
+                if (location.getWorld() == null) return;
+
+                TNTPrimed tnt = (TNTPrimed) location.getWorld().spawnEntity(location, EntityType.PRIMED_TNT);
+
+                int fuseTicks = config.getInt("Items.Skeletor.TNTOnArrowHit.TNTTimer");
+                double tntYield = config.getDouble("Items.Skeletor.TNTOnArrowHit.TNTYield");
+                boolean cancelTNT = config.getBoolean("Items.Skeletor.TNTOnArrowHit.TNTDoesNoBlockDamage");
+
+                if (fuseTicks < 1) {
+                    new VBLogger(getClass().getName(), Level.WARNING, "Skeletor TNT Fuse ticks were set too low. 20 ticks is one second, cannot be less than 1 tick. Ticks: " + fuseTicks).logToFile();
+                } else {
+                    tnt.setFuseTicks(fuseTicks);
+                }
+
+                if (tntYield < 0) {
+                    new VBLogger(getClass().getName(), Level.WARNING, "Skeletor TNT yield was set too low. Cannot be less than 0. Yield: " + tntYield).logToFile();
+                } else {
+                    tnt.setYield((float) tntYield);
+                }
+
+                if (cancelTNT) {
+                    tnt.getScoreboardTags().add(CANCEL_TNT_EXPLOSION);
+                }
+            }
+        }
+
+        /**
+         * Cancels the explosion of primed tnt if it has the scoreboardTag intended for this
+         *
+         * @param event the event to cancel the explosion in
+         */
+        @EventHandler
+        public void onTNTExplosion(EntityExplodeEvent event) {
+
+            boolean isCancelledTNT = event.getEntity() instanceof TNTPrimed
+                    && event.getEntity().getScoreboardTags().contains(CANCEL_TNT_EXPLOSION);
+
+            if (isCancelledTNT) {
+                event.setCancelled(true);
+            }
+
+        }
     }
 }
