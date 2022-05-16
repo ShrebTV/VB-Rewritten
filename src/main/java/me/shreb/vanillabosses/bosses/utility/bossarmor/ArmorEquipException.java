bb40ee23ecaeaf8db28db58bb92e1571ededf7f5
Shreb <ShrebTV@gmail.com>
2022-05-16 09:49:31 +0200

    Added some documentation

--- src/main/java/me/shreb/vanillabosses/bosses/utility/bossarmor/ArmorEquipException.java
+++ src/main/java/me/shreb/vanillabosses/bosses/utility/bossarmor/ArmorEquipException.java
@@ -1,5 +1,8 @@
 package me.shreb.vanillabosses.bosses.utility.bossarmor;
 
+/**
+ * This exception indicates a problem of any kind related to equipping armor onto a Living Entity
+ */
 public class ArmorEquipException extends Exception{
 
     public ArmorEquipException(String s){
